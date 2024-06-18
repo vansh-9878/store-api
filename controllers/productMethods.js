f062cf1b50e0b5198437551183669d88dd41be7a
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-06-18 17:27:41 +0530

    Create productMethods.js

---
+++ controllers/productMethods.js
@@ -0,0 +1,63 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ name: "vase table" });
+  res.status(200).json({ products, nbHites: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObj = {};
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regex = /\b(<|>|>=|<|<=|=)\b/g;
+    let filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.include(field)) {
+        queryObj[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Product.find(queryObj);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  const products = await result;
+
+  res.status(200).json({ products, nbHites: products.length });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
